180b817a64577f0a3c00c030e488dc58af304739
Luis <tu-correo@ejemplo.com>
2026-08-10 20:59:12 -0500

    Revalidation token ok

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  UseGuards,
-  Header,
-  Headers,
-  SetMetadata,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
-import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
-import { RawHeaders, GetUser, RoleProtected, Auth } from './decorators';
-// import { IncomingHttpHeaders } from 'http';
-import { UserRoleGuard } from './guards/user-role.guard';
-import { ValidRoles } from './interfaces';
+import { GetUser, Auth } from './decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -27,63 +14,13 @@ export class AuthController {
   }
 
   @Post('login')
-  LoginUser(@Body() loginUserDto: LoginUserDto) {
+  loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
 
-  // @Get('privateOne')
-  // @UseGuards(AuthGuard)
-  // testPrivateRuote1(@GetUser(['email', 'role', 'fullName']) user: User) {
-  //   // 1. don't send the token
-  //   // 2. invalid token
-  //   // 2. send token
-  //   // 3. send token with a inactive user
-  //   console.log({ user });
-  //   return {
-  //     ok: true,
-  //     user,
-  //   };
-  // }
-
-  @Get('private2')
-  @UseGuards(AuthGuard)
-  testPrivateRuote(
-    @GetUser() user: User,
-    @GetUser('email') userEmail: string,
-    @RawHeaders() rawHeaders: string[],
-    // @Headers() headers:IncomingHttpHeaders
-  ) {
-    // 1. don't send the token
-    // 2. invalid token
-    // 2. send token
-    // 3. send token with a inactive user
-    return {
-      ok: true,
-      userEmail,
-      user,
-      rawHeaders,
-    };
-  }
-
-  @Get('private')
-  // @RoleProtected() // asi cualquiera
-  @RoleProtected(ValidRoles.user)
-  @UseGuards(AuthGuard, UserRoleGuard)
-  // @SetMetadata('roles', ['admin', 'super-user'])
-  testPrivateRuoteThree(@GetUser() user: User) {
-    return {
-      ok: true,
-      user,
-    };
-  }
-
-  @Get('private3')
-  // @Auth(ValidRoles.admin)
-  @Auth() // sin roles | debe estar autenticado
-  testPrivateRuoteFour(@GetUser() user: User) {
-    return {
-      ok: true,
-      user,
-    };
+  @Get('check-status')
+  @Auth()
+  public checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -65,6 +65,13 @@ export class AuthService {
     };
   }
 
+  async checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
   private handleDbErrors(error: any): never {
     if (error.code === '2305')
       throw new BadRequestException(`Error ${error.detail}`);
